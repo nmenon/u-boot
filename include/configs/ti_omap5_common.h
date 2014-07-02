d3f82da6baf2e2ea7d4c18f0afb4e73cc85add04
Nishanth Menon <nm@ti.com>
2014-07-02 09:20:15 -0500

    TI: OMAP5: add dtb name for DRA72-evm
    DRA72-evm is a new platform using DRA72x Silicon. Add support for dtb
    name in findfdt
    
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- include/configs/ti_omap5_common.h
+++ include/configs/ti_omap5_common.h
@@ -116,6 +116,8 @@
 			"setenv fdtfile omap5-uevm.dtb; fi; " \
 		"if test $board_name = dra7xx; then " \
 			"setenv fdtfile dra7-evm.dtb; fi;" \
+		"if test $board_name = dra72x; then " \
+			"setenv fdtfile dra72-evm.dtb; fi;" \
 		"if test $fdtfile = undefined; then " \
 			"echo WARNING: Could not determine device tree to use; fi; \0" \
 	"loadfdt=load mmc ${bootpart} ${fdtaddr} ${bootdir}/${fdtfile};\0" \
