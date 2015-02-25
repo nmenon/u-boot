f1a7d77ef59468390886460688dc027850474c97
Praveen Rao <prao@ti.com>
2015-02-25 10:06:01 -0600

    ARM: DRA7 / OMAP5: Add workaround for ARM errata 798870
    This patch enables the workaround for ARM errata 798870 for OMAP5 /
    DRA7 which says "If back-to-back speculative cache line fills (fill
    A and fill B) are issued from the L1 data cache of a CPU to the
    L2 cache, the second request (fill B) is then cancelled, and the
    second request would have detected a hazard against a recent write or
    eviction (write B) to the same cache line as fill B then the L2 logic
    might deadlock."
    
    An l2auxctlr accessor implementation for OMAP5 and DRA7 is introduced
    here as well.
    
    Signed-off-by: Praveen Rao <prao@ti.com>
    Signed-off-by: Angela Stegmaier <angelabaker@ti.com>
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- arch/arm/include/asm/arch-omap5/sys_proto.h
+++ arch/arm/include/asm/arch-omap5/sys_proto.h
@@ -66,4 +66,8 @@ static inline u32 usec_to_32k(u32 usec)
 {
 	return div_round_up(32768 * usec, 1000000);
 }
+
+void omap_smc1(u32 service, u32 val);
+#define OMAP5_SERVICE_L2ACTLR_SET    0x104
+
 #endif
